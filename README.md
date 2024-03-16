63c1527bf6a482ae480f1b7fd69348c34d618f59
Nirina Rabeson <nirina.rabeson@zenika.com>
2024-03-16 14:43:20 +0100

    feat: bun package

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# rina-rs
+# ni-rs
 
 This project is like <https://github.com/antfu/ni> but in rust, because why not?
+
+## Build
+
+```sh
+cargo build --workspace --bins -r && cp target/release/na target/release/ni npm/dist
+```
---
+++ npm/bin/ni.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env bun
+const proc = Bun.spawn(
+  [require.resolve("../dist/ni"), ...process.argv.slice(2)],
+  {
+    stdout: "pipe",
+  },
+);
+
+for await (const chunk of proc.stdout) {
+  process.stdout.write(new TextDecoder().decode(chunk));
+}
