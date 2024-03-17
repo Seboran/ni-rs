73a6e9c91a5cdd4280c9124899a5fa55069e6bc2
Nirina Rabeson <nirina.rabeson@zenika.com>
2024-03-17 14:01:01 +0100

    fix: permet de run les packages sans passer par javascript

--- npm/bin/na.cjs
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const { spawn } = require("node:child_process");
-
-const ls = spawn(require.resolve("../dist/na"), process.argv.slice(2));
-
-ls.stdout.on("data", (/** @type {string | Uint8Array} */ data) => {
-  process.stdout.write(data);
-});
--- npm/bin/ni.cjs
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const { spawn } = require("node:child_process");
-
-const ls = spawn(require.resolve("../dist/ni"), process.argv.slice(2));
-
-ls.stdout.on("data", (/** @type {string | Uint8Array} */ data) => {
-  process.stdout.write(data);
-});
