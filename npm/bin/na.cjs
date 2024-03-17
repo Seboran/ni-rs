a7285dbaffb80c6a45eccc21b34f5ae3dddbf548
Nirina Rabeson <nirina.rabeson@zenika.com>
2024-03-17 13:34:20 +0100

    fix: permet de run en tant que cjs

--- npm/bin/na.cjs
+++ npm/bin/na.cjs
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
-import { spawn } from "child_process";
+const { spawn } = require("node:child_process");
+
 const ls = spawn(require.resolve("../dist/na"), process.argv.slice(2));
 
-ls.stdout.on("data", (data) => {
+ls.stdout.on("data", (/** @type {string | Uint8Array} */ data) => {
   process.stdout.write(data);
 });
