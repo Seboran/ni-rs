ef007e08822745e33d7bf23e970fb34df3a576b4
Nirina Rabeson <nirinarabeson@me.com>
2024-03-17 20:22:39 +0000

    feat: ajout apple darwin build

--- cross-build.sh
+++ cross-build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-targets=("x86_64-unknown-linux-gnu" "x86_64-pc-windows-gnu")
+targets=("x86_64-unknown-linux-gnu" "x86_64-pc-windows-gnu" "aarch64-apple-darwin")
 for target in "${targets[@]}"
 do
   cross build --target $target --workspace --bins -r
 done
-#"aarch64-apple-darwin"
